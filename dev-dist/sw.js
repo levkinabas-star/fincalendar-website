65e2b2da421b56d0d90aef37e58ab542d7ad1aab
FinCalendar User <user@promto.ai>
2026-04-27 10:00:01 +0000

    Update service worker and dev build output
    Made with promto.ai (user: 944541183, conversation: b450ef81-b25c-4da0-8972-9875b28fd461)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f7c39696'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mt2hv5dudmo"
+    "revision": "0.dgvhcbuafhg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
